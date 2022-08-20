54f5909453cd6054ead7f13a083ac1128d5d94a3
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-19 21:12:48 -0700

    clean up

--- andariego-menu-server/index.js
+++ andariego-menu-server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 const ITEMS_PATH = require('./routes/api/items');
 const CATEGORIES_PATH = require('./routes/api/categories');
 
