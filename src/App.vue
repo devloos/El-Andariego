f0aae93922dbc0a1bfabd001f1d252245fe7ebca
Carlos Aguilera <18aguilerac@gmail.com>
2022-09-22 14:45:19 -0700

    path and footer fix

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+  overflow-x: hidden;
 }
 #main {
   min-height: 100vh;
