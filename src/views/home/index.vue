578a80aaeeb1ae75bd36194afaf08bf18c9a98ba
Carlos Aguilera <18aguilerac@gmail.com>
2023-01-17 14:02:22 -0800

    carousel slider update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,45 +2,29 @@
   <div class="container-fluid">
     <hero-section />
     <platillos-slider />
-    <div class="row px-2 mt-5">
-      <div
-        id="Home-Slider"
-        class="col-12 col-lg-10 col-xl-8 carousel carousel-dark slide mx-auto d-block mb-5"
-        data-bs-ride="carousel"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="/home/slider/Logo.jpeg" class="img-fluid" alt="..." />
-          </div>
-          <div v-for="(image, i) in sliderImages" :key="i" class="carousel-item">
-            <img :src="image" class="img-fluid" alt="..." />
-          </div>
+
+    <div id="img-slider" class="carousel slide px-2 my-5" data-bs-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-bs-target="#img-slider" data-bs-slide-to="0" class="active"></li>
+        <li data-bs-target="#img-slider" data-bs-slide-to="1"></li>
+        <li data-bs-target="#img-slider" data-bs-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img-slider.png" class="img-fluid" alt="slider-img" />
         </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#Home-Slider"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#Home-Slider"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        </button>
+        <div v-for="(image, i) in sliderImages" :key="i" class="carousel-item">
+          <img :src="image" class="img-fluid" alt="slider-img" />
+        </div>
       </div>
     </div>
 
     <category-selector />
 
-    <div class="row px-2">
+    <div class="row px-2 my-5">
       <div class="col-12">
         <hr class="mx-auto m-2 border border-dark border-1" style="width: 5rem" />
-        <p class="text-center text-muted fs-6">Find El Andariego</p>
+        <p class="text-center text-muted">Find El Andariego</p>
       </div>
       <div class="col-12 col-lg-10 col-xl-8 mx-auto">
         <img
@@ -67,7 +51,7 @@ export default {
   },
   data() {
     return {
-      sliderImages: ['/home/slider/Tacos.jpeg', '/home/slider/Cubana.png'],
+      sliderImages: ['img-slider-1.webp', 'img-slider.png'],
     };
   },
 };
