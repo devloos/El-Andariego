47ea0da806e3be37d1f829c90e39510c4d612c34
Carlos Aguilera <18aguilerac@gmail.com>
2023-05-13 11:33:26 -0700

    update

--- src/components/smart-divider.vue
+++ src/components/smart-divider.vue
@@ -10,8 +10,15 @@ export default {
 </script>
 
 <template>
-  <div class="mt-5 mb-4">
+  <div class="text-center text-muted mt-5 mb-4">
     <hr class="mx-auto m-2 border border-dark border-1" style="width: 5rem" />
-    <p class="text-center text-muted" v-text="divider_name"></p>
+    <p class="divider-text" v-text="divider_name"></p>
   </div>
 </template>
+
+<style>
+.divider-text {
+  text-transform: uppercase;
+  font-size: 14px;
+}
+</style>
