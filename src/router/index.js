986acb8bbce95725b0bf2835db7495468aecfe01
Carlos Aguilera <18aguilerac@gmail.com>
2023-05-26 21:23:26 -0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Menu from '@/views/Menu.vue';
 import Contact from '@/views/Contact.vue';
@@ -51,7 +51,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
