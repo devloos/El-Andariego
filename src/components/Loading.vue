2e69369a00310e83e36fe347276e0bc064298592
Carlos Aguilera <18aguilerac@gmail.com>
2023-05-27 20:57:50 -0700

    update

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="flex h-96 w-full items-center justify-center">
-    <p class="text-9xl tracking-widest">...</p>
+    <span class="loader"></span>
   </div>
 </template>
+
+<style>
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 5px solid #333;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
