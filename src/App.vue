307ca8b0568651a4ca96bfb82fac59e628866227
Carlos Aguilera <18aguilerac@gmail.com>
2023-05-27 21:21:26 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { useHead } from '@vueuse/head';
 import AndariegoNav from '@/components/nav/AndariegoNav.vue';
 import AndariegoFooter from '@/components/AndariegoFooter.vue';
+import { useRoute } from 'vue-router';
 
 useHead({
   title: 'El Andariego',
@@ -12,18 +13,32 @@ useHead({
     },
   ],
 });
+
+const route = useRoute();
 </script>
 
 <template>
   <AndariegoNav />
-  <main>
-    <RouterView />
-  </main>
+  <RouterView v-slot="{ Component }" :key="route.path" class="router-link">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
   <AndariegoFooter />
 </template>
 
 <style scoped>
-main {
+.router-link {
   min-height: calc(100vh - 460px);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
