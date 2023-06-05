03449ad845b2c458572d64f309c9caeeecef607a
Carlos Aguilera <18aguilerac@gmail.com>
2023-06-04 17:21:48 -0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -47,6 +47,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 export default router;
