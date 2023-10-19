e1cf83bac87a48dce88e4e8bda486cc1f2b7246b
Carlos Aguilera <18aguilerac@gmail.com>
2023-10-18 22:49:13 -0700

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,36 +7,38 @@ const Blog = () => import('@/views/Blog.vue');
 const Post = () => import('@/views/Post.vue');
 const Error = () => import('@/views/Error.vue');
 
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/menu/:id',
+    name: 'Menu',
+    component: Menu,
+  },
+  {
+    path: '/contact',
+    component: Contact,
+  },
+  {
+    path: '/blog',
+    component: Blog,
+  },
+  {
+    path: '/blog/:id',
+    component: Post,
+    props: true,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: Error,
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: Home,
-    },
-    {
-      path: '/menu/:id',
-      name: 'Menu',
-      component: Menu,
-    },
-    {
-      path: '/contact',
-      component: Contact,
-    },
-    {
-      path: '/blog',
-      component: Blog,
-    },
-    {
-      path: '/blog/:id',
-      component: Post,
-      props: true,
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      component: Error,
-    },
-  ],
+  routes,
   scrollBehavior(to, from, savedPosition) {
     return new Promise((resolve) => {
       if (savedPosition) {
