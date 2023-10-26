18a40dda00679bd115cb8828e3d9f6a31f5f8364
Carlos Aguilera <18aguilerac@gmail.com>
2023-10-25 23:09:38 -0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(to, _, savedPosition) {
     return new Promise((resolve) => {
       if (savedPosition) {
         resolve(savedPosition);
