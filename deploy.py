77b0e9e51c67ed6127e099713d3688a07bcc95a1
Carlos Aguilera <18aguilerac@gmail.com>
2024-02-09 23:55:03 -0800

    deploy

--- deploy.py
+++ deploy.py
@@ -1,9 +1,7 @@
 import os
 from git import Repo
 
-
-ANDARIEGO_VUE_PATH = '/Users/ca/Developer/Projects/El-Andariego/el-andariego-vue'
-ANDARIEGO_API_PATH = '/Users/ca/Developer/Projects/El-Andariego/el-andariego-api'
+ANDARIEGO_PROJECT_PATH = '/Users/ca/Developer/Projects/El-Andariego'
 
 BRANCH = 'main'
 
@@ -13,30 +11,25 @@ answer = input("Are you sure you want to deploy? (yes/no) [no]: ")
 if (answer != 'yes'):
     exit(0)
 
-if (Repo(ANDARIEGO_VUE_PATH).active_branch.name != BRANCH):
-    print(f"You are not on {BRANCH} for Andariego Vue! [FAILED]")
+if (Repo(ANDARIEGO_PROJECT_PATH).active_branch.name != BRANCH):
+    print(f"You are not on {BRANCH} for Andariego Project! [FAILED]")
     exit(1)
-
-if (Repo(ANDARIEGO_API_PATH).active_branch.name != BRANCH):
-    print(f"You are not on {BRANCH} for Andariego API! [FAILED]")
-    exit(1)
-
-os.system('rm -rf dist')
 
 # in el-andariego-vue
-os.chdir(ANDARIEGO_VUE_PATH)
-os.system(f"git pull origin {BRANCH}")
+os.chdir(ANDARIEGO_PROJECT_PATH + '/client')
 os.system('npm run build')
 
 if int(os.popen('echo $?').read()) != 0:
     print('Error building Andariego Vue! [FAILED]')
     exit(1)
 
-os.system(f"mv dist {ANDARIEGO_API_PATH}")
+os.system(f"mv dist {ANDARIEGO_PROJECT_PATH + '/server'}")
 
-# in el-andariego-api
-os.chdir(ANDARIEGO_API_PATH)
+os.chdir(ANDARIEGO_PROJECT_PATH)
 os.system('git add .')
 os.system('git commit -m "deploy"')
 os.system('git push')
+
+# in el-andariego-api
+os.chdir(ANDARIEGO_PROJECT_PATH + '/server')
 os.system('railway up --detach')
