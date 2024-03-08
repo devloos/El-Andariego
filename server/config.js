df13dcc5935733f9c4deed6751b0d8c084571022
Carlos Aguilera <18aguilerac@gmail.com>
2024-03-07 17:23:24 -0800

    test

--- server/config.js
+++ server/config.js
@@ -6,6 +6,8 @@ dotenv.config({
   path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`),
 });
 
+console.log("hello");
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
